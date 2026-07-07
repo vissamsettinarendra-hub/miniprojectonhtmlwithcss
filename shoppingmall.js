b2a4af30a9675cf57d2816e98046ccac45262c26
vissamsettinarendra-hub <vissamsettinarendra@gmail.com>
2026-07-07 09:35:01 +0530

    initial js

---
+++ shoppingmall.js
@@ -0,0 +1,12 @@
+const products=[
+{id:1,name:"Laptop",category:"Electronics",price:50000},
+{id:2,name:"Mouse",category:"Electronics",price:600},
+{id:3,name:"Keyboard",category:"Electronics",price:1200},
+{id:4,name:"Pen",category:"Stationery",price:20},
+{id:5,name:"Notebook",category:"Stationery",price:80},
+{id:6,name:"Bag",category:"Accessories",price:1500},
+{id:7,name:"Watch",category:"Accessories",price:3000},
+{id:8,name:"Mobile",category:"Electronics",price:25000},
+{id:9,name:"Scale",category:"Stationery",price:40},
+{id:10,name:"Headphones",category:"Electronics",price:2500}
+];
